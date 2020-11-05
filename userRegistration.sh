52fc4f3b4a5e510cadf9fcab9f00c8e4cb460d51
nida.j <nida.jawre@gmail.com>
2020-11-05 22:32:53 +0530

    Added: First name validation code

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 echo "User Registration System needs to ensure all the validation"
+echo "-------First Name Validation----------"
+read -p "Enter First Name: " fName
+pat_F="^[[:upper:]]{1}[[:lower:]]{2,}$"
+echo $pat
+if [[ $fName =~ $pat_L ]]
+then
+        echo "Valid"
+else
+        echo "Invalid"
+fi
